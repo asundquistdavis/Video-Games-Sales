c312acd4bede7eb41c9a3077eda025ad4867035a
Grimsbear <107369678+Grimsbear@users.noreply.github.com>
2022-09-13 19:20:41 -0500

    ERD add and ETL change

--- schema.sql
+++ schema.sql
@@ -1,9 +1,43 @@
--- "table1" schema
+CREATE TABLE "video_games" (
+    "game_id" int   NOT NULL,
+    "name" varchar(30)   NOT NULL,
+    "platform_id" int   NOT NULL,
+    CONSTRAINT "pk_video_games" PRIMARY KEY (
+        "game_id"
+     )
+);
 
--- "table2" schema
+CREATE TABLE "platform" (
+    "platform_id" int   NOT NULL,
+    "platform" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_platform" PRIMARY KEY (
+        "platform_id"
+     )
+);
 
--- select * from "table1"
+CREATE TABLE "sales" (
+    "sales_id" int   NOT NULL,
+    "game_id" int   NOT NULL,
+    "region_id" int   NOT NULL,
+    "sales" int   NOT NULL,
+    CONSTRAINT "pk_sales" PRIMARY KEY (
+        "sales_id"
+     )
+);
 
--- select * from "table2"
+CREATE TABLE "population" (
+    "region_id" int   NOT NULL,
+    "population" int   NOT NULL,
+    CONSTRAINT "pk_population" PRIMARY KEY (
+        "region_id"
+     )
+);
 
--- join statements
+ALTER TABLE "video_games" ADD CONSTRAINT "fk_video_games_platform_id" FOREIGN KEY("platform_id")
+REFERENCES "platform" ("platform_id");
+
+ALTER TABLE "sales" ADD CONSTRAINT "fk_sales_game_id" FOREIGN KEY("game_id")
+REFERENCES "video_games" ("game_id");
+
+ALTER TABLE "sales" ADD CONSTRAINT "fk_sales_region_id" FOREIGN KEY("region_id")
+REFERENCES "population" ("region_id");
