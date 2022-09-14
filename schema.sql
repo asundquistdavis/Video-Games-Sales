c7f1e11922f92960483734de04cb0fe4d0bdd9ea
Grimsbear <107369678+Grimsbear@users.noreply.github.com>
2022-09-13 21:13:29 -0500

    schema.sql update and finished off the load portion of the etl

--- schema.sql
+++ schema.sql
@@ -1,43 +1,28 @@
 CREATE TABLE "video_games" (
-    "game_id" int   NOT NULL,
-    "name" varchar(30)   NOT NULL,
-    "platform_id" int   NOT NULL,
+    "Game_Id" int   NOT NULL,
+    "Name" varchar(255)   NOT NULL,
+    "Platform" varchar(255)   NOT NULL,
     CONSTRAINT "pk_video_games" PRIMARY KEY (
-        "game_id"
-     )
-);
-
-CREATE TABLE "platform" (
-    "platform_id" int   NOT NULL,
-    "platform" varchar(30)   NOT NULL,
-    CONSTRAINT "pk_platform" PRIMARY KEY (
-        "platform_id"
-     )
-);
-
-CREATE TABLE "sales" (
-    "sales_id" int   NOT NULL,
-    "game_id" int   NOT NULL,
-    "region_id" int   NOT NULL,
-    "sales" int   NOT NULL,
-    CONSTRAINT "pk_sales" PRIMARY KEY (
-        "sales_id"
+        "Game_Id"
      )
 );
 
 CREATE TABLE "population" (
-    "region_id" int   NOT NULL,
-    "population" int   NOT NULL,
+    "Region" varchar(2)   NOT NULL,
+    "Population" bigint   NOT NULL,
     CONSTRAINT "pk_population" PRIMARY KEY (
-        "region_id"
+        "Region"
      )
 );
 
-ALTER TABLE "video_games" ADD CONSTRAINT "fk_video_games_platform_id" FOREIGN KEY("platform_id")
-REFERENCES "platform" ("platform_id");
+CREATE TABLE "sales" (
+    "Game_Id" int   NOT NULL,
+    "Region" varchar(2)   NOT NULL,
+    "Sales" bigint   NOT NULL
+);
 
-ALTER TABLE "sales" ADD CONSTRAINT "fk_sales_game_id" FOREIGN KEY("game_id")
-REFERENCES "video_games" ("game_id");
+ALTER TABLE "sales" ADD CONSTRAINT "fk_sales_Game_Id" FOREIGN KEY("Game_Id")
+REFERENCES "video_games" ("Game_Id");
 
-ALTER TABLE "sales" ADD CONSTRAINT "fk_sales_region_id" FOREIGN KEY("region_id")
-REFERENCES "population" ("region_id");
+ALTER TABLE "sales" ADD CONSTRAINT "fk_sales_Region" FOREIGN KEY("Region")
+REFERENCES "population" ("Region");
